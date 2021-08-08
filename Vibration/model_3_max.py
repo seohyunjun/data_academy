16d1935352ecb86d2c6f31464a081b8b345ec5d6
seohyunjun <bnmy6581@gmail.com>
2021-08-08 16:00:41 +0900

    Max model Validation Apply

--- Vibration/model_3_max.py
+++ Vibration/model_3_max.py
@@ -11,7 +11,7 @@ import numpy as np
 
 OR_PATH = 'C:\\Users\\admin\\Desktop\\code'
 DATA_PATH = 'D:\\기계시설물 고장 예지 센서\\Training'
-
+VALIDATION_PATH = 'D:\\기계시설물 고장 예지 센서\\Validation'
 ## 회사
 #DATA_PATH = 'D:\\project\\기계시설물 고장 예지 센서\\Training'
 
@@ -160,4 +160,90 @@ plt.legend()
 plt.show()
 os.chdir(DATA_PATH)
 
+############ Validation #####################################################################
 
+os.chdir(VALIDATION_PATH)
+
+## 정상 : 4195 VS 비정상 : 2043
+val_normal_peak, val_abnormal_peak = total_peak_max_load(type,kw,machine,state,ab_state,200)
+
+val_normal = val_normal_peak
+val_abnormal = val_abnormal_peak
+
+
+np_val_normal = np.array(val_normal).reshape(len(val_normal)//60,60)
+np_val_abnormal = np.array(val_abnormal).reshape(len(val_abnormal)//60,60)
+
+#reshape
+val_normal = np_val_normal.reshape(np_val_normal.shape[0],1,np_val_normal.shape[1])
+val_abnormal = np_val_abnormal.reshape(np_val_abnormal.shape[0],1,np_val_abnormal.shape[1])
+
+print("Validataion Normal Data shape:", val_normal.shape)
+print("Validataion Abnormal Data shape:", val_abnormal.shape)
+
+normal = 1
+abnormal = 0
+
+pd_val_normal = pd.DataFrame(np_val_normal)
+pd_val_abnormal = pd.DataFrame(np_val_abnormal)
+
+pd_val_normal['label'] = np.ones(len(np_val_normal))
+pd_val_abnormal['label'] = np.zeros(len(np_val_abnormal))
+
+Validation = pd.concat([pd_val_normal,pd_val_abnormal])
+
+# Shuffle Data
+Validation = Validation.sample(frac=1)  
+
+# Model Load
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+sns.set(color_codes=True)
+
+os.chdir(OR_PATH)
+model = tf.keras.models.load_model(f'model/{type}_{kw}_{machine}_{ab_state}_Scale_Min_Max(max).h5')
+os.chdir(VALIDATION_PATH)
+
+input_val = np.array(Validation.iloc[:,:-1]).reshape(len(Validation),1,60)
+
+scaler = MinMaxScaler()
+input_val = scaler.fit_transform(input_val)
+input_val = input_val.reshape(len(input_val),1,60)
+
+#0.03
+val_predict = model.predict(input_val)
+val_predict = val_predict.reshape(val_predict.shape[0],val_predict.shape[2])
+input_val = input_val.reshape(input_val.shape[0],input_val.shape[2])
+scored = pd.DataFrame()
+scored['Loss_mae'] = np.mean(np.abs(input_val-val_predict), axis=1)
+scored['label'] = Validation['label'].values
+
+plt.figure(figsize=(16,9), dpi=80)
+plt.title('Loss Distribution', fontsize=16)
+sns.distplot(scored['Loss_mae'], bins = 20, kde= True, color = 'blue')
+plt.show()
+
+# Threshold = 0.04
+#pred = []
+#for i in scored['Loss_mae']:
+#    if 0.02<i<0.04 or 0.05<i<0.1:
+#        pred.append(1)
+#    else:
+#        pred.append(0)
+pred = []
+for i in scored['Loss_mae']:
+    if i<0.05:
+        pred.append(1)
+    else:
+        pred.append(0)
+
+scored['pred'] = pred
+
+plt.scatter(range(len(scored)),scored['label'],label='Actual')
+plt.scatter(range(len(scored)),scored['pred'],label='Pred')
+plt.show()
+
+sum(scored['pred']==scored['label'])/len(scored) ## 99.7% 
