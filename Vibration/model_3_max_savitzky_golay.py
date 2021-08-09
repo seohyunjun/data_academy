ad4877899382e25a29daa1c8ed341c754539c1b6
seohyunjun <bnmy6581@gmail.com>
2021-08-10 01:43:02 +0900

    confusion matrix

--- Vibration/model_3_max_savitzky_golay.py
+++ Vibration/model_3_max_savitzky_golay.py
@@ -14,9 +14,6 @@ DATA_PATH = 'D:\\기계시설물 고장 예지 센서\\Training'
 VALIDATION_PATH = 'D:\\기계시설물 고장 예지 센서\\Validation'
 ## 회사
 #DATA_PATH = 'D:\\project\\기계시설물 고장 예지 센서\\Training'
-#VALIDATION_PATH = 'D:\\기계시설물 고장 예지 센서\\Validation'
-#OR_PATH = 'C:\\Users\\A\\Desktop\\code'
-
 
 os.chdir(DATA_PATH)
 type = 'vibration'
@@ -27,12 +24,20 @@ ab_state = '회전체불평형'
 
 # 정상 : 33552
 # 회전체불평형 : 16336
-normal_peak, abnormal_peak = total_peak_max_load(type,kw,machine,state,ab_state,200)
+normal_peak, abnormal_peak = total_peak_load(type,kw,machine,state,ab_state,200)
 
 # reshape
 np_normal_peak = np.array(normal_peak).reshape(len(normal_peak)//60,60)
 np_abnormal_peak = np.array(abnormal_peak).reshape(len(abnormal_peak)//60,60)
 
+
+# savgol_filter
+from scipy.signal import savgol_filter
+
+
+
+plt.plot(np.fft.fft(np_normal_peak[0]).real)
+plt.show()
 from sklearn.preprocessing import MinMaxScaler
 #from sklearn.externals import joblib
 import joblib
@@ -45,13 +50,16 @@ X_test = np_abnormal_peak
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-scaler_filename = f'{type}_{kw}_{machine}_{ab_state}_scaler(max)'
+scaler_filename = "scaler_data"
 joblib.dump(scaler, scaler_filename)
 
 #reshape
 X_train = X_train.reshape(X_train.shape[0],1,X_train.shape[1])
 X_test = X_test.reshape(np_abnormal_peak.shape[0],1,np_abnormal_peak.shape[1])
 
+
+X_train = X_train[:10000]
+X_test = X_test[:10000]
 print("Training data shape:", X_train.shape)
 print("Test data shape:", X_test.shape)
 
@@ -94,12 +102,12 @@ history = model.fit(X_train, X_train, epochs=nb_epochs, batch_size=batch_size,
 
 
 os.chdir(OR_PATH)
-model.save(f'model/{type}_{kw}_{machine}_{ab_state}_Scale_Min_Max(max).h5')
+model.save(f'model/{type}_{kw}_{machine}_{ab_state}_Scale Min_Max.h5')
 plt.figure(figsize=(15,10))
 plt.plot(history['loss'],label='loss')
 plt.legend()
-plt.title(f'{type}_{kw}_{machine}_{ab_state} Model Loss Plot Scale Min Max(max)')
-plt.savefig(f'model_plot/{type}_{kw}_{machine}_{ab_state}_Model_Loss_Plot_Scale_Min_Max(max).jpg',dpi=300)
+plt.title(f'{type}_{kw}_{machine}_{ab_state} Model Loss Plot Scale Min Max')
+plt.savefig(f'model_plot/{type}_{kw}_{machine}_{ab_state}_Model_Loss_Plot_Scale_Min_Max.jpg',dpi=300)
 plt.show()
 os.chdir(DATA_PATH)
 
@@ -113,18 +121,21 @@ scored = pd.DataFrame(index=X_pred.index)
 Xtrain = X_train.reshape(X_train.shape[0], X_train.shape[2])
 scored['Loss_mae'] = np.mean(np.abs(X_pred-Xtrain), axis = 1)
 
+
+np.percentile(scored['Loss_mae'],75)
+np.median(scored['Loss_mae'])
 os.chdir(OR_PATH)
 plt.figure(figsize=(16,9), dpi=80)
 plt.title(f'{type} {kw} {machine} {ab_state} Loss Distribution', fontsize=16)
 sns.distplot(scored['Loss_mae'], bins = 20, kde= True, color = 'blue')
-plt.savefig(f'model_plot/{type}_{kw}_{machine}_{ab_state}_Loss_Distribution_ScaleMinMax(max).jpg',dpi=300)
+plt.savefig(f'model_plot/{type}_{kw}_{machine}_{ab_state}_Loss_Distribution_ScaleMinMax.jpg',dpi=300)
 #plt.xlim([0.007,0.008])
 plt.show()
 
 plt.figure(figsize=(16,9), dpi=80)
 plt.title(f'{type} {kw} {machine} {ab_state} Loss Scatter Plot', fontsize=16)
-sns.scatterplot(range(len(scored)),scored['Loss_mae'])
-plt.savefig(f'model_plot/{type}_{kw}_{machine}_{ab_state}_Loss_Scatter_ScaleMinMax(max)2.jpg',dpi=300)
+plt.scatter(range(len(scored)),scored['Loss_mae'])
+plt.savefig(f'model_plot/{type}_{kw}_{machine}_{ab_state}_Loss_Scatter_ScaleMinMax.jpg',dpi=300)
 plt.show()
 os.chdir(DATA_PATH)
 
@@ -143,25 +154,15 @@ test_X_pred = pd.DataFrame(test_X_pred)
 test_scored = pd.DataFrame(index=test_X_pred.index)
 Xtest = X_test.reshape(X_test.shape[0], X_test.shape[2])
 test_scored['Loss_mae'] = np.mean(np.abs(test_X_pred-Xtest), axis = 1)
-
-###test scored scatter plot
-os.chdir(OR_PATH)
-plt.figure(figsize=(16,9), dpi=80)
-plt.title(f'{type} {kw} {machine} {ab_state} Loss MAE Test Scatter Plot', fontsize=16)
-sns.scatterplot(range(len(test_scored)),test_scored['Loss_mae'])
-plt.savefig(f'model_plot/{type}_{kw}_{machine}_{ab_state}_Test_Loss_Scatter_ScaleMinMax(max)2.jpg',dpi=300)
-plt.show()
-os.chdir(DATA_PATH)
-
-test_scored['Threshold'] = 0.04
-Threshold1 = 0.021
-Threshold2 = 0.031
+test_scored['Threshold'] = np.percentile(scored['Loss_mae'],99)
 
 #min(test_scored['Loss_mae'])
 
-test_scored['Anomaly'] = [0 if i>Threshold1 and i<Threshold2 else 1 for i in test_scored['Loss_mae']]
-test_scored.head()
-(len(test_scored)-sum(test_scored['Anomaly']))/len(test_scored)
+# Normal = 1 Anomaly = 0
+test_scored['Anomaly'] = test_scored['Loss_mae'].apply(lambda x: 0 if x > np.percentile(scored['Loss_mae'],99) else 1)
+
+(len(test_scored)-sum(test_scored['Anomaly']))/len(test_scored) # 100%
+
 # 0: Pred_Anomaly 1: Pred_Normal 
 os.chdir(OR_PATH)
 plt.figure(figsize=(15,9))
@@ -173,12 +174,14 @@ plt.legend()
 plt.show()
 os.chdir(DATA_PATH)
 
+
+
 ############ Validation #####################################################################
 
 os.chdir(VALIDATION_PATH)
 
 ## 정상 : 4195 VS 비정상 : 2043
-val_normal_peak, val_abnormal_peak = total_peak_max_load(type,kw,machine,state,ab_state,200)
+val_normal_peak, val_abnormal_peak = total_peak_load(type,kw,machine,state,ab_state,200)
 
 val_normal = val_normal_peak
 val_abnormal = val_abnormal_peak
@@ -226,7 +229,7 @@ scaler = MinMaxScaler()
 input_val = scaler.fit_transform(input_val)
 input_val = input_val.reshape(len(input_val),1,60)
 
-#0.03
+#.03
 val_predict = model.predict(input_val)
 val_predict = val_predict.reshape(val_predict.shape[0],val_predict.shape[2])
 input_val = input_val.reshape(input_val.shape[0],input_val.shape[2])
@@ -246,17 +249,16 @@ plt.show()
 #        pred.append(1)
 #    else:
 #        pred.append(0)
-pred = []
-for i in scored['Loss_mae']:
-    if i<0.05:
-        pred.append(1)
-    else:
-        pred.append(0)
-
-scored['pred'] = pred
+scored['pred'] = scored['Loss_mae'].apply(lambda x: 0 if x>np.percentile(scored['Loss_mae'],99) else 1)
 
 plt.scatter(range(len(scored)),scored['label'],label='Actual')
 plt.scatter(range(len(scored)),scored['pred'],label='Pred')
 plt.show()
 
-sum(scored['pred']==scored['label'])/len(scored) ## 99.7% 
+sum(scored['pred']==scored['label'])/len(scored) ## 66.23% 
+
+#Validation confusion matrix
+from sklearn.metrics import confusion_matrix
+scored['label'] = scored['label'].apply(lambda x: int(x))
+confusion_matrix(scored['label'],scored['pred'])
+co
